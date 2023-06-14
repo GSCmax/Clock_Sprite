dcffc7f51d99ea308741a4ddd77005edd2846913
gsc <gsc067218@gmail.com>
2023-06-14 08:54:02 +0800

    change startup location

--- Clock_Sprite/App.xaml.cs
+++ Clock_Sprite/App.xaml.cs
@@ -34,8 +34,8 @@ namespace Clock_Sprite
                 ms.Show();
 
                 ms.Left = ss[i].WorkingArea.Right - ms.ActualWidth - offset;
-                //ms.Top = ss[i].WorkingArea.Top + offset;
-                ms.Top = ss[i].WorkingArea.Bottom - ms.ActualHeight - offset;
+                ms.Top = ss[i].WorkingArea.Top + offset; //右上角
+                //ms.Top = ss[i].WorkingArea.Bottom - ms.ActualHeight - offset; //右下角
                 WindowAttach.SetSnapDistance(ms, offset);
                 ms.title_Run.Text = $"Clock Sprite [{i + 1}/{ss.Length}] 使用技巧：";
 
