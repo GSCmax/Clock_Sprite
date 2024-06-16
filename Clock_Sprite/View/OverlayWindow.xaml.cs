4a61015b0a8f4594083bdcc5f559691a962e3454
gsc <gsc067218@gmail.com>
2024-06-16 14:54:00 +0800

    继续开发

--- Clock_Sprite/View/OverlayWindow.xaml.cs
+++ Clock_Sprite/View/OverlayWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media.Animation;
 
 namespace Clock_Sprite.View
@@ -12,8 +14,26 @@ namespace Clock_Sprite.View
         public OverlayWindow()
         {
             InitializeComponent();
+
+            SourceInitialized += delegate
+            {
+                IntPtr hwnd = new WindowInteropHelper(this).Handle;
+                uint extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+                SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+            };
+
             StartBlinking();
         }
+
+        private const int WS_EX_TRANSPARENT = 0x20;
+
+        private const int GWL_EXSTYLE = -20;
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(IntPtr hwnd, int nIndex);
 
         private void StartBlinking()
         {
